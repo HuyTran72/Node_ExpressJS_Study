3b0413460a7ffc4cea3293b9233837c3319b3e89
Huy Tran <baohuy82.th@gmail.com>
2024-11-28 01:24:36 -0800

    Update index.js

--- ReadingFiles/index.js
+++ ReadingFiles/index.js
@@ -11,3 +11,7 @@ fs.readFile('./data.json', 'utf-8', (err, data) => {
 	var received = JSON.parse(data)
 	console.log(received.address)
 })
+
+fs.readdir('../', (err, data) => {
+	console.log(data)
+})
